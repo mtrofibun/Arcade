05c96ffb70cf836c98426ea54840591542bf3e33
NinaTrofimov <ntrofimov1@my.waketech.edu>
2025-06-15 20:14:21 -0400

    updated

--- script.js
+++ script.js
@@ -1,16 +1,60 @@
 let questioncounter = 0;
 
 const questions = [{
-    question : 'Question 1',
+    question : 'Q1 : You step into a buzzing arcade full of flashing lights and noise. Do you:',
     type : 'EI',
-    option1 : 'Hello',
-    option2 : 'Hello2'
+    option1 : 'Jump right into the middle of the action! The flashy bright colors have already caught your attention',
+    option2 : 'Analyze the layout, you want to find the games that suit your own rhythm'
 },
 {
-    question : 'Question 2',
+    question : 'Q2 :  Before you start playing, do you:',
+    type : 'JP',
+    option1 : 'Take a moment to figure out what games you should spend your tokens on, there is no rush',
+    option2 : 'Wander freely, letting whatever catches your eye guide your play'
+},
+{
+    question : 'Q3: When you pick a game to play, what do you prefer:',
+    type : 'SN',
+    option1 : `I prefer games with straight forward paths, it's more enjoyable seeing immediate results!`,
+    option2 : 'Games that invite you to explore patterns, and unique strategies are more fun!'
+
+},
+{
+    question : 'Q4 : Faced with many options, what do you usually choose?',
+    type : 'SN',
+    option1 : 'I want to stick with a familiar game it is good to know exactly what to expect and how to win',
+    option2 : 'I would like to try a brand-new game, the more challenging it is the better!'
+},
+{
+    question : 'Q5 : While waiting your turn on a popular game you spot a group of people',
     type : 'EI',
-    option1 : 'Option1',
-    option2 : 'option2'
+    option1 : 'Strike up a conversation with other players, to pass the time I like to share tips',
+    option2 : 'Silently observe the current player, I am too focused on the game to notice the group up ahead'
+
+},
+{
+    question : `Q6 : It's finally your turn! You're on the track of setting a new record that a crowd has appeared around you!`,
+    type : 'EI',
+    option1 : `The pressure is on, you can't disappoint your newly made fans!`,
+    option2 : `All these eyes on you make you feel like you're going to lose!`
+},
+{
+    question : `Q7 : Oh no! It seems like you've lost,`,
+    type : 'TF',
+    option1 : ``,
+    option2 : ``
+},
+{
+    question : 'Time to spend your tickets!',
+    type : 'JP',
+    option1 : `I'll come back next time, better to save my tickets for a bigger yet better prize!`,
+    option2 : `Candy, stamps, fake tattoos! I don't want to leave the arcade empty handed!`
+},
+{
+    question : 'Seems like the arcade has closed, what was the most memorable part?',
+    type : 'SN',
+    option1 : 'The exact details — like the timing, layout, and mechanics of the games',
+    option2 : 'The amazing plot and storyline, the characters outshined the games!'
 },
 {
     question : 'Question 3',
@@ -20,15 +64,8 @@ const questions = [{
 },
 
 
-
 ]
 
-let types = [
-  ['E', 'I'],
-  ['S', 'N'],
-  ['T', 'F'],
-  ['J', 'P']
-];
 
 let scores = {
   E: 0, I: 0,
@@ -38,21 +75,27 @@ let scores = {
 };
 
 const optionButtons = document.querySelector('#group-buttons');
+audios_array = [ '/graphics/Button.mp3','/graphics/Button2.mp3','/graphics/Button3.mp3','/graphics/Button4.mp3','/graphics/Button5.mp3']
+
 
 optionButtons.addEventListener('click', event =>{
     let mbtitype = ''
+    audio_picked = audios_array[Math.floor(Math.random() * audios_array.length)]
+
     if(event.target.id == "option-one"){
-        console.log('one');
         mbtitype = questions[questioncounter].type.charAt(0)
         scores[mbtitype] += 1;
 
     }
     else if(event.target.id == "option-two"){
-        console.log('two');
         mbtitype = questions[questioncounter].type.charAt(1)
         scores[mbtitype] += 1;
     }
+
+    let audio = new Audio(audio_picked)
+    audio.play();
     questioncounter += 1;
+    console.log(scores)
     questionFunction();
 })
 
@@ -74,9 +117,25 @@ function results(){
     document.getElementById('questions').style.display = 'none';
     document.getElementById('option-one').style.display = 'none';
     document.getElementById('option-two').style.display= 'none';
-    let mbti = '';
-    
+
+    let mbtiarr = [];
+    types = ['E','I','S','N','T','F','J','P']
+/* find a more better time complexity/code for this plsss oki ty future me */
+    for(let i = 0; i < types.length -1; i++){
+        if(scores[types[i]] > scores[types[i+1]]){
+            mbtiarr.push(types[i])
+        }
+        else{
+            mbtiarr.push(types[i+1])
+        }
+        i += 1;
+    }
+    let mbti = mbtiarr.join('')
+
     console.log(mbti)
+    document.getElementById('results').style.display = 'block';
+    document.getElementById('result-test').textContent = mbti;
+    
 }
 
 
